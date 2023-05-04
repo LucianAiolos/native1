559c51f34f9f7c6b3ab2f5e4d4f4de7ba31f6df7
Lucian Aiolos <luxianaiolos@gmail.com>
2023-05-04 12:14:37 -0700

    created redux reducer, store, actions

--- type.d.ts
+++ type.d.ts
@@ -1,5 +1,16 @@
-interface IArticle {
-  id: number;
-  title: string;
-  body: string;
+
+interface IUser {
+  id: string
+  name: string
 }
+
+type UserAction = {
+  type: string
+  user: IUser
+}
+
+type UserState = {
+  user: IUser
+}
+
+type DispatchType = (args: ArticleAction) => ArticleAction
